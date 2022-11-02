05514e17377fba803fceb9a5423d37831ee0540c
SankaraMoothi <sankaramoorthi99@gmail.com>
2022-11-02 15:22:31 +0530

    fourthcommit

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function App() {
         "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
     },
   ]);
+
   let AddMovie = (e) => {
     setMovielist([...Movielist, e]);
   };
--- src/Component/DashBoard/index.js
+++ src/Component/DashBoard/index.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DashStylee.css";
+import Button from "@mui/material/Button";
+import { ButtonGroup } from "@mui/material";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 function DashBoard({ elementee }) {
+  const [sam, setSam] = useState(true);
+  let handleSummary = () => {
+    setSam(!sam);
+  };
   return (
     <>
       {elementee.map((els) => (
-        <div className="col-lg-3 col-md-4 col-sm-6 mb-4 vvv">
-          <div className="card" style={{ width: "18rem" }}>
+        <div className="col-lg-3 col-md-6 col-sm-6 mb-4 vvv">
+          <div className="card" style={{ width: "25rem" }}>
             <img
               className="card-img-top img"
               src={els.poster}
@@ -15,13 +25,42 @@ function DashBoard({ elementee }) {
             />
             <div className="card-body">
               <span className="card-title">
-                <h4>{els.name}</h4>
+                <h4>
+                  {els.name}
+                  <Button onClick={() => handleSummary()}>
+                    <i class="fa-solid fa-angle-down"></i>
+                  </Button>
+                </h4>
+                <span style={{ color: els.rating > 8 ? "green" : "red" }}>
+                  ⭐ {els.rating}
+                </span>
               </span>
-              <span>Rating: ⭐ {els.rating}</span>
-              <p className="card-text">{els.summary}</p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
+              {sam ? <p className="card-text">{els.summary}</p> : <></>}
+              <div className="FooterButton">
+                <ButtonGroup
+                  variant="outlined"
+                  aria-label="outlined primary button group"
+                >
+                  <Button
+                    variant="outlined"
+                    startIcon={<ThumbUpOutlinedIcon />}
+                  ></Button>
+                  <Button
+                    variant="outlined"
+                    startIcon={<ThumbDownOutlinedIcon />}
+                  ></Button>
+                </ButtonGroup>
+                <ButtonGroup
+                  variant="outlined"
+                  aria-label="outlined primary button group"
+                >
+                  <Button variant="outlined" startIcon={<EditIcon />}></Button>
+                  <Button
+                    variant="outlined"
+                    startIcon={<DeleteIcon />}
+                  ></Button>
+                </ButtonGroup>
+              </div>
             </div>
           </div>
         </div>
