375d43b1a7d6da8ef5058dd10b343589ec0400be
SankaraMoothi <sankarmoorthi99@gmail.com>
2023-05-17 08:38:30 +0530

    userUpdate

--- src/Component/CardCreate/index.js
+++ src/Component/CardCreate/index.js
@@ -12,14 +12,18 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Badge from "@mui/material/Badge";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../UseContext";
 export default function CardCreate({ data, id, deleteMovie }) {
   const styles = {
     color: data.rating >= 8.5 ? "green" : "red",
   };
   const navigate = useNavigate();
+  const { userInfo } = React.useContext(UserContext);
   const [show, setshow] = React.useState(true);
   const [like, setlike] = React.useState(0);
   const [dislike, setdislike] = React.useState(0);
+  let creater = data?.userName;
+  let Username = creater.slice(0, 4);
   return (
     <Card sx={{ maxWidth: 345, marginBottom: 3 }}>
       <CardActionArea>
@@ -73,12 +77,18 @@ export default function CardCreate({ data, id, deleteMovie }) {
         </div>
 
         <div>
-          <IconButton onClick={() => navigate(`MovieEdit/${data._id}`)}>
-            <EditIcon />
-          </IconButton>
-          <IconButton sx={{ color: "red" }} onClick={() => deleteMovie(id)}>
-            <DeleteIcon />
-          </IconButton>
+          {data?.userId === userInfo.id ? (
+            <>
+              <IconButton onClick={() => navigate(`MovieEdit/${data._id}`)}>
+                <EditIcon />
+              </IconButton>
+              <IconButton sx={{ color: "red" }} onClick={() => deleteMovie(id)}>
+                <DeleteIcon />
+              </IconButton>
+            </>
+          ) : (
+            <>{!Username ? "" : Username}</>
+          )}
         </div>
       </CardActions>
     </Card>
