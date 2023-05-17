2e933517caef3d7cf51bb677f1435362fc49e69a
SankaraMoothi <sankarmoorthi99@gmail.com>
2023-05-17 09:25:10 +0530

    uuuuu

--- src/Component/Login/index.jsx
+++ src/Component/Login/index.jsx
@@ -21,6 +21,7 @@ function Login() {
     };
     const users = await fetch(`${API}/user/login`, {
       method: "POST",
+      mode: "cors",
       body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
